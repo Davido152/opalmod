3a572dfa4d0a2d0a360cf3fb6460d1ce09ac8f31
Davido152 <74314163+Davido152@users.noreply.github.com>
2021-04-16 19:14:49 +0200

    Add files via upload

--- src/main/java/github/davido152/opalmod/util/Reference.java
+++ src/main/java/github/davido152/opalmod/util/Reference.java
@@ -5,7 +5,7 @@ public class Reference
 	
 	public static final String MOD_ID = "opal_mod";
 	public static final String NAME = "Opal Mod";
-	public static final String VERSION = "0.0.5";
+	public static final String VERSION = "0.0.7";
 	public static final String ACCEPTED_VERSIONS = "[1.12.2]";
 	public static final String CLIENT_PROXY_CLASS = "github.davido152.opalmod.proxy.ClientProxy";
 	public static final String COMMON_PROXY_CLASS = "github.davido152.opalmod.proxy.CommonProxy";
