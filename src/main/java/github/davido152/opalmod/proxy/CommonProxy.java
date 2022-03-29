13019d3a126eae82773456623cf852460a8f8af6
Davido152 <74314163+Davido152@users.noreply.github.com>
2022-03-29 19:56:24 +0200

    Delete src/main directory

--- src/main/java/github/davido152/opalmod/proxy/CommonProxy.java
+++
@@ -1,8 +0,0 @@
-package github.davido152.opalmod.proxy;
-
-import net.minecraft.item.Item;
-
-public class CommonProxy 
-{
-	public void registerItemRenderer(Item item, int meta, String id) {}
-}
--- src/main/java/github/davido152/opalmod/util/interfaces/IHasModel.java
+++
@@ -1,6 +0,0 @@
-package github.davido152.opalmod.util.interfaces;
-
-public interface IHasModel 
-{	
-	public void registerModels();
-}
